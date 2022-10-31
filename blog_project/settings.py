4ea578333f3f67095863f54395040a4c51e81e35
Ahmed Hammam <39192401+ahmedhammamm@users.noreply.github.com>
2022-10-31 21:38:44 +0200

    Update settings.py

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os    #new 
 
 from pathlib import Path
+from environs import Env
+
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-as8cr$jzga48ckv@2p$vfk03@+$(wbd04$&113l0@@cro=q*17'
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
